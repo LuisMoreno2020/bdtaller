1dc0c02c37d033d87c7943418c79ebc5d96c616d
Luis Moreno <lamscl@hotmail.com>
2020-10-24 13:02:35 -0300

    Actualizacion de Script

---
+++ script.sql
@@ -0,0 +1,183 @@
+/*SCRIPT PROYECTO*/
+
+CREATE DATABASE if not exists Biblioteca;
+
+USE Biblioteca;
+
+CREATE TABLE if not exists Cargo (
+	CodCargo int not null,
+	NombreCargo varchar(25) not null,
+	CONSTRAINT PK_CARGO_CodCargo
+	PRIMARY KEY (CodCargo)
+	);
+
+CREATE TABLE if not exists Usuario (
+	RutUsuario int not null,
+	DvUsuario varchar(1) not null,
+	Nombres varchar(25) not null,
+	ApellidoPaterno varchar(25) not null,
+	ApellidoMaterno varchar(25) not null,
+	Email varchar(35) null,
+	Cargo int not null,
+	Activo int(1) not null, 
+	Contraseña varchar(15) not null,
+	CONSTRAINT PK_Usuario_RutUsuario
+	PRIMARY KEY (RutUsuario)
+	CONSTRAINT FK_Usuario_Cargo
+	FOREIGN KEY (Cargo) reference Cargo(idCargo)
+	);
+
+CREATE TABLE if not exists TelefonoUsuario (
+	IdTelefono int not null,
+	Telefono int not null,
+	RutUsuario int not null,
+	CONSTRAINT PK_TelefonoUsuario_Idtelefono
+	PRIMARY KEY (IdTelefono)
+	CONSTRAINT FK_TelefonoUsuario_RutUsuario
+	FOREIGN KEY (RutUsuario) reference Usuario(RutUsuario)
+	);
+
+CREATE TABLE if not exists Usuario_Cargo (
+	RutUsuario int not null,
+	CodCargo int not null,
+	CONSTRAINT FK_Usuario_Cargo_RutUsuario
+	FOREIGN KEY (RutUsuario) reference Usuario(RutUsuario)
+	CONSTRAINT FK_Usuario_Cargo_CodCargo
+	FOREIGN KEY (CodCargo) reference Cargo(CodCargo)
+	);
+
+CREATE TABLE if not exists Log (
+	RutUsuario int not null,
+	Movimiento varchar(30) not null,
+	Fecha date not null default,
+	CONSTRAINT FK_Log_RutUsuario
+	FOREIGN KEY (RutUsuario) reference Usuario(RutUsuario)
+	);
+
+CREATE TABLE if not exists Curso (
+	CodCurso int not null,
+	Curso int(1) not null,
+	CONSTRAINT PK_Curso_CodCurso
+	PRIMARY KEY (CodCurso)
+	);
+
+CREATE TABLE if not exists Letra (
+	CodLetra int not null,
+	Letra varchar(1) not null,
+	CONSTRAINT PK_Letra_CodLetra
+	PRIMARY KEY (CodLetra)
+	);
+
+CREATE TABLE if not exists Nivel (
+	CodNivel int not null,
+	Nivel varchar(10) not null,
+	CONSTRAINT PK_Nivel_CodNivel
+	PRIMARY KEY (CodNivel)
+	);
+
+
+CREATE TABLE if not exists Alumno (
+	RutAlumno int not null,
+	DvAlumno varchar(1) not null,
+	Nombres varchar(25) not null,
+	ApellidoPaterno varchar(25) not null,
+	ApellidoMaterno varchar(25) not null,
+	Calle varchar(35) not null,
+	Numero int not null,
+	CodCurso int not null,
+	CodLetra int not null,
+	CodNivel int not null,
+	CONSTRAINT PK_Alumno_RutAlumno
+	PRIMARY KEY (RutAlumno)
+	CONSTRAINT FK_Alumno_CodCurso
+	FOREIGN KEY (CodCurso) reference Curso(CodCurso)
+	CONSTRAINT FK_Alumno_CodLetra
+	FOREIGN KEY (CodLetra) reference Letra(CodLetra)
+	CONSTRAINT FK_Alumno_CodNivel
+	FOREIGN KEY (CodNivel) reference Nivel(CodNivel)
+	);
+
+CREATE TABLE if not exists TelefonoAlumno (
+	IdTelefono int not null,
+	Telefono int not null,
+	RutAlumno int not null,
+	CONSTRAINT PK_TelefonoAlumno_Idtelefono
+	PRIMARY KEY (IdTelefono)
+	CONSTRAINT FK_TelefonoAlumno_RutAlumno
+	FOREIGN KEY (RutAlumno) reference Usuario(RutAlumno)
+	);
+
+CREATE TABLE if not exists Prestamo (
+	Nro int not null,
+	RutUsuario int not null,
+	DvUsuario varchar(1) not null,
+	RutAlumno int not null,
+	DvAlumno varchar(1) not null,
+	Emision date not null,
+	CONSTRAINT PK_Prestamo_Nro
+	PRIMARY KEY (Nro)
+	CONSTRAINT FK_Prestamo_RutUsuario
+	FOREIGN KEY (RutUsuario) reference Usuario(RutUsuario)
+	CONSTRAINT FK_Prestamo_RutAlumno
+	FOREIGN KEY (RutAlumno) reference Alumno(RutAlumno)
+	);
+
+CREATE TABLE if not exists Autor (
+	CodAutor int not null,
+	NombreAutor varchar (30),
+	CONSTRAINT PK_autor_CodAutor
+	PRIMARY KEY (CodAutor)
+	);
+
+CREATE TABLE if not exists Categoria (
+	CodCategoria int not null,
+	NombreCategoria varchar(30)
+	CONSTRAINT PK_Categoria_CodCategoria
+	PRIMARY KEY (CodCategoria)
+	);
+
+CREATE TABLE if not exists Libro (
+	Isbn varchar(13) not null,
+	Titulo varchar(100) not null,
+	NroPagina int not null,
+	Autor int not null,
+	CodCategoria int not null,
+	CONSTRAINT PK_libro_Isbn
+	PRIMARY KEY(Isbn)
+	CONSTRAINT FK_Autor_CodAutor
+	FOREIGN KEY (CodAutor) reference Autor(CodAutor)
+	CONSTRAINT FK_Autor_CodCategoria
+	FOREIGN KEY (CodCategoria) reference Categoria(CodCategoria)
+	);
+
+CREATE TABLE if not exists Ubicacion (
+CodUbicacion int not null,
+NombreUbicacion varchar(25),
+CONSTRAINT PK_Ubicacion_CodUbicacion
+PRIMARY KEY (CodUbicacion)
+);
+
+CREATE TABLE if not exists Ejemplar (
+	CodEjemplar int not null,
+	CodUbicacion int not null,
+	Estado int not null,
+	Activo int not null,
+	CONSTRAINT PK_ejemplar_CodEjemplar
+	PRIMARY KEY (CodEjemplar)
+	CONSTRAINT PK_Ejemplar_Ubicacion
+	FOREIGN KEY(CodUbicacion) reference Ubicacion(CodUbicacion)
+	);
+
+
+CREATE TABLE if not exists DetallePrestamo (
+	NroPrestamo int not null,
+	Codigo int(15) not null,
+	DiasPropuestos int not null,
+	FechaDevPropuesta date not null default,
+	Comentario text null,
+	FechaDevolucionReal date not null default,	
+	CONSTRAINT FK_DetallePrestamo_NroPrestamo
+	FOREIGN KEY (NroPrestamo) reference Prestamo(Nro)
+	CONSTRAINT FK_DetallePrestamo_Codigo
+	FOREIGN KEY (Codigo) reference Ejemplar(CodEjemplar)
+	);
